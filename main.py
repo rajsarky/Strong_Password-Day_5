e58d0a4af65a7c4d6729dbc18ca541b0183a6ca2
kai <k.n.sarkar@icloud.com>
2024-01-18 18:53:08 +0000

    password generator - works

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+random.shuffle(letters)
+random.shuffle(symbols)
+random.shuffle(numbers)
 
+password = ""
+for i in range(nr_letters):
+  password += random.choice(letters)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for i in range(nr_symbols):
+  password += random.choice(symbols)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(nr_numbers):
+  password += random.choice(numbers)
+
+char_list = list(password)
+random.shuffle(char_list)
+password = "".join(char_list)
+
+print(password)
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
